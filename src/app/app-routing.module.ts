645bf44df3ea5bafb9b188387738fe2f391c24db
lion <breakermoob@hotmail.com>
2021-09-26 00:12:35 -0500

    components removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { APP_BASE_HREF } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { EmptyRouteComponent } from "./empty-route/empty-route.component";
-import { BeforeclickComponent } from "./prueba/beforeclick/beforeclick.component";
 import { DefaultComponent } from "./prueba/default/default.component";
 
 const routes: Routes = [
-  { path: "", component: DefaultComponent },
-  { path: "prueba", component: BeforeclickComponent },
-  { path: "**", component: EmptyRouteComponent }];
+  { path: "", component: DefaultComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
